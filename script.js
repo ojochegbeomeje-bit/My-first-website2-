43b441e3de2af362a50e27bc533620e556650184
ojochegbeomeje-bit <ojochegbeomeje@gmail.com>
2026-02-07 08:54:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+alert("Welcome to my website!");
+function changeText() {
+  document.getElementById("message").innerText = "JavaScript is working";
+}
+function changeText() {
+  document.getElementById("message").innerText =
+    "JavaScript is working perfectly 🚀";
+}
+function toggleTheme() {
+  document.body.classList.toggle("light-mode");
+}
